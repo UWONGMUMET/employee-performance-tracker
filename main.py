bbba790cffb7f170e644cae00bc413dd5147a81e
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-30 15:05:26 +0700

    feat: register leaderboard routes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from core.config import settings
 
-from routers import auth, user, dashboard, department, performance_review
+from routers import auth, user, dashboard, department, performance_review, leaderboard
 
 app = FastAPI(
     title="Employee Performance Tracker API",
@@ -13,6 +13,7 @@ app.include_router(user.router)
 app.include_router(dashboard.router)
 app.include_router(department.router)
 app.include_router(performance_review.router)
+app.include_router(leaderboard.router)
 
 @app.get("/")
 def root():
